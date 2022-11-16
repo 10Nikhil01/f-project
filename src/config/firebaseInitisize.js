7fc061e435b40e6efe100cca9bf53c29d372e989
NIKHIL KUMAR <108527011+10Nikhil01@users.noreply.github.com>
2022-11-16 14:25:46 +0530

    .

--- src/config/firebaseInitisize.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import {getAuth} from "firebase/auth";
-import firebaseConfig from "./firebase";
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth();
